60e168c5de9ac212419895c70760c592d502e0f6
Neil <mea@bexleyinter.net>
2026-02-24 14:29:15 +0000

    EDIT: Add Kirsty and update to release version 2.0

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("Neil"));
+        System.out.println(sayHelloToSomeone("Kirsty"));
         System.out.println(sayGoodbye());
     }
 
